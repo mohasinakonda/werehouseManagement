09ca39b505944394b75ca8b0a63af1e3474006c5
mohasin akonda <mohasinakonda39@gmail.com>
2022-05-01 15:00:55 +0600

    env file updated

--- src/RequeireAuth/RequireAuth.js
+++ src/RequeireAuth/RequireAuth.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyB_ImtnCDRdcDTWlq8xhHnE81XF3ST999w",
-	authDomain: "laptop-stock-market-3f076.firebaseapp.com",
-	projectId: "laptop-stock-market-3f076",
-	storageBucket: "laptop-stock-market-3f076.appspot.com",
-	messagingSenderId: "440049873550",
-	appId: "1:440049873550:web:f1f31e2d38a38204174fdf",
+	apiKey: process.env.local.SECRET_APP_KEY,
+	authDomain: process.env.local.AUTH_DOMAIN,
+	projectId: process.env.local.PROJECT_ID,
+	storageBucket: process.env.local.STORAGE_BUCKET,
+	messagingSenderId: process.env.local.MESSAGING_SENDER_ID,
+	appId: process.env.local.APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-//pass=ou0bigiYlrQmEjEu
-//dblaptopuser
+const auth = getAuth(app)
